413d5bc1498d4e867665bba09ecd3d31a656cee7
alineberry <ablineberry@gmail.com>
2025-01-21 13:52:40 -0700

    Update version to 0.0.10 in pyproject.toml and enhance CI workflow
    - Incremented the package version in pyproject.toml from 0.0.9 to 0.0.10 for the upcoming release.
    - Added test dependencies and a new development dependency (Jupyter) to the poetry configuration.
    - Improved CI workflow by adding steps to install dependencies and run tests, ensuring better integration and testing processes.
    - Introduced a new Jupyter notebook for development purposes.
    - Refactored import statements in db_utils.py and utils.py for consistency.
    
    These changes enhance the package's functionality, testing capabilities, and overall development experience.

---
+++ tests/test_ohlcv.py
@@ -0,0 +1,30 @@
+import pytest
+import pandas as pd
+import copy
+import pandas.testing as tm
+
+from phantom_core.ohlcv import fill_ohlcv
+
+@pytest.fixture
+def ohlcv_df() -> pd.DataFrame:
+    index = pd.date_range(start='2024-03-20 09:30:00', periods=5, freq='5min')
+    df = pd.DataFrame({
+        'volume': [1000, 1500, 800, 2000, 1200],
+        'vwap': [150.25, 150.50, 150.40, 150.75, 150.60],
+        'transactions': [50, 75, 40, 100, 60],
+        'close': [150.30, 150.55, 150.35, 150.80, 150.65],
+        'open': [150.20, 150.45, 150.45, 150.70, 150.55],
+        'high': [150.35, 150.60, 150.50, 150.85, 150.70],
+        'low': [150.15, 150.40, 150.30, 150.65, 150.50],
+        'avg_size': [20, 20, 20, 20, 20],
+        'ticker': ['AAPL', 'AAPL', 'AAPL', 'AAPL', 'AAPL']
+    }, index=index)
+    return copy.deepcopy(df)
+
+
+class TestFillOHLCV:
+
+    def test_1(self, ohlcv_df: pd.DataFrame):
+        expected = copy.deepcopy(ohlcv_df)
+        actual = fill_ohlcv(ohlcv_df)
+        tm.assert_frame_equal(actual, expected)
